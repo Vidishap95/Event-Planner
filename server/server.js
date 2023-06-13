4c622fcc3ad22ce79cb167f5609dc4cec06843f3
Amala Hema <amalahema@gmail.com>
2023-06-13 12:56:49 -0400

    folder added

--- server/server.js
+++ server/server.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const app = express();
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () =>
 {
     console.log("Server is running on PORT 3001!");
