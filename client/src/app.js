9964fa291120e868c389177bc31255a1d82215d5
Leo Liu <aleoliu96@gmail.com>
2023-06-14 13:50:09 -0400

    added Hero

--- client/src/app.js
+++ client/src/app.js
@@ -1,9 +1,11 @@
 import NavBar from "./components/NavBar";
+import Hero from "./components/Hero";
 
 function App() {
   return (
     <div>
       <NavBar />
+      <Hero />
     </div>
   );
 }
---
+++ client/src/components/Hero.js
@@ -0,0 +1,24 @@
+import React from 'react'
+
+const Hero = () => {
+    return (
+        <section className="bg-white dark:bg-gray-900">
+            <div class="py-8 px-4 mx-auto max-w-screen-xl text-center lg:py-16 lg:px-12">
+                <h1 class="mb-4 text-4xl font-extrabold tracking-tight leading-none text-gray-900 md:text-5xl lg:text-6xl dark:text-white">Welcome to Flawless Function</h1>
+                <p class="mb-8 text-lg font-normal text-gray-500 lg:text-xl sm:px-16 xl:px-48 dark:text-gray-400">Flawlessly planning all your functions();</p>
+                <div class="flex flex-col mb-8 lg:mb-16 space-y-4 sm:flex-row sm:justify-center sm:space-y-0 sm:space-x-4">
+                    <a href="#" class="inline-flex justify-center items-center py-3 px-5 text-base font-medium text-center text-black rounded-lg bg-primary-700 hover:bg-primary-800 focus:ring-4 focus:ring-primary-300 dark:focus:ring-primary-900">
+                        Login
+                        <svg class="ml-2 -mr-1 w-5 h-5" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
+                    </a>
+                    <a href="#" class="inline-flex justify-center items-center py-3 px-5 text-base font-medium text-center text-gray-900 rounded-lg border border-gray-300 hover:bg-gray-100 focus:ring-4 focus:ring-gray-100 dark:text-white dark:border-gray-700 dark:hover:bg-gray-700 dark:focus:ring-gray-800">
+                        <svg class="mr-2 -ml-1 w-5 h-5" viewBox="0 0 20 20" ></svg>
+                        Sign Up!
+                    </a>
+                </div>
+            </div>
+        </section>
+    )
+}
+
+export default Hero
