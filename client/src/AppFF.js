c5c3e1e2de8a2554849b981af8b8bdf266399ff7
Leo Liu <aleoliu96@gmail.com>
2023-06-14 14:26:18 -0400

    changed App.js => AppFF.js

--- client/src/AppFF.js
+++ client/src/AppFF.js
@@ -4,12 +4,12 @@ import NavBar from "./components/NavBar";
 import Hero from "./components/Hero";
 
 function App() {
-  return (
-    <div>
-      <NavBar />
-      <Hero />
-    </div>
-  );
+    return (
+        <div>
+            <NavBar />
+            <Hero />
+        </div>
+    );
 }
 
 export default App;
