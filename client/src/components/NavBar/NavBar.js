a9af2b90e75e0bd95d3f19b7d75f8be3e953f651
Leo Liu <aleoliu96@gmail.com>
2023-06-19 12:08:24 -0400

    more steamlined UI

--- client/src/components/NavBar/NavBar.js
+++ client/src/components/NavBar/NavBar.js
@@ -11,30 +11,30 @@ const NavBar = () => {
                     <img src={FFLogo} className="h-8 mr-3" alt="FF Logo" />
                     <span className="self-center lg:text-3xl font-semibold whitespace-nowrap sm:text-sm text-yellow-100"> Flawless Function</span>
                 </Link>
-                <button data-collapse-toggle="navbar-default" type="button" className="inline-flex items-center p-2 ml-3 text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200" aria-controls="navbar-default" aria-expanded="false">
+                <button data-collapse-toggle="navbar-default" type="button" className="inline-flex items-center p-2 ml-3 text-sm text-yellow-100 rounded-lg  border-r-yellow-950 md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200" aria-controls="navbar-default" aria-expanded="false">
                     <span className="sr-only">Open main menu</span>
                     <svg className="w-6 h-6" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20"><path fill-rule="evenodd" d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z" clip-rule="evenodd"></path></svg>
                 </button>
                 <div className="hidden w-full md:block md:w-auto" id="navbar-default">
-                    <ul className="font-medium flex flex-col p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 md:mt-0 md:border-0 md:bg-white md:dark:bg-gray-900 ">
+                    <ul className="font-medium flex flex-col p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-transparent md:flex-row md:space-x-8 md:mt-0 md:border-0 md:bg-trans md:dark:bg-gray-900 ">
                         <li>
-                            <Link to="/" className="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0">Home</Link>
+                            <Link to="/" className="block py-2 pl-3 pr-4 text-yellow-100 rounded hover:text-yellow-950 md:hover:bg-transparent md:border-0 md:hover:text-yellow-950 md:p-0">Home</Link>
                         </li>
                         <li>
-                            <Link to="/Events" className="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0">Events</Link>
+                            <Link to="/Events" className="block py-2 pl-3 pr-4 text-yellow-100 rounded hover:text-yellow-950 md:hover:bg-transparent md:border-0 md:hover:text-yellow-950 md:p-0">Events</Link>
                         </li>
                         <li>
-                            <Link to="/Profile" className="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0">Profile</Link>
+                            <Link to="/Profile" className="block py-2 pl-3 pr-4 text-yellow-100 rounded hover:text-yellow-950 md:hover:bg-transparent md:border-0 md:hover:text-yellow-950 md:p-0">Profile</Link>
                         </li>
                         <li>
-                            <Link to="/Login" className="inline-flex justify-center items-center py-3 px-5 text-base font-medium text-center text-black rounded-lg bg-primary-700 hover:bg-primary-800 focus:ring-4 focus:ring-primary-300">
+                            <Link to="/Login" className="block py-2 pl-3 pr-4 text-yellow-100 rounded hover:text-yellow-950 md:hover:bg-transparent md:border-0 md:hover:text-yellow-950 md:p-0">
                                 Login
-                                <svg className="ml-2 -mr-1 w-5 h-5" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
+                                {/* <svg className="ml-2 -mr-1 w-5 h-5" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg> */}
                             </Link>
                         </li>
                         <li>
-                            <Link to="/SignUp" className="inline-flex justify-center items-center py-3 px-5 text-base font-medium text-center text-gray-900 rounded-lg border border-gray-300 hover:bg-gray-100 focus:ring-4 focus:ring-gray-100">
-                                <svg className="mr-2 -ml-1 w-5 h-5" viewBox="0 0 20 20" ></svg>
+                            <Link to="/SignUp" className="block py-2 pl-3 pr-4 text-yellow-100 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0">
+                                {/* <svg className="mr-2 -ml-1 w-5 h-5" viewBox="0 0 20 20" ></svg> */}
                                 Sign Up!
                             </Link>
                         </li>
--- client/src/pages/Signup.js
+++ client/src/pages/Signup.js
@@ -41,12 +41,12 @@ const Signup = () => {
   };
 
   return (
-    <section className='bg-gray-50'>
+    <section className='bg-transparent'>
       <div className='flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0'>
-        <main className="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0">
+        <main className="w-full bg-yellow-950 rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0">
           <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
-            <div className="card text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl">
-              <h4 className="card-header text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl">Create an Account</h4>
+            <div className="card text-xl font-bold leading-tight tracking-tight bg-yellow-950 md:text-2xl">
+              <h4 className="card-header text-xl font-bold leading-tight tracking-tight text-yellow-100 md:text-2xl">Create an Account</h4>
               <div className="card-body">
                 {data ? (
                   <p>
@@ -55,18 +55,18 @@ const Signup = () => {
                   </p>
                 ) : (
                   <form onSubmit={handleFormSubmit}>
-                    <label className='block mb-2 text-sm font-medium text-gray-900'>
-                      Your Username
+                    <label className='block mb-2 text-sm font-medium text-yellow-100'>
+                      Your Name
                     </label>
                     <input
                       className="form-input bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5"
-                      placeholder="Your username"
+                      placeholder="Your name"
                       name="name"
                       type="text"
                       value={formState.name}
                       onChange={handleChange}
                     />
-                    <label className='block mb-2 text-sm font-medium text-gray-900'>
+                    <label className='block mb-2 text-sm font-medium text-yellow-100'>
                       Your Email
                     </label>
                     <input
@@ -77,7 +77,7 @@ const Signup = () => {
                       value={formState.email}
                       onChange={handleChange}
                     />
-                    <label className='block mb-2 text-sm font-medium text-gray-900'>
+                    <label className='block mb-2 text-sm font-medium text-yellow-100'>
                       Your Password
                     </label>
                     <input
@@ -89,7 +89,7 @@ const Signup = () => {
                       onChange={handleChange}
                     />
                     <button
-                      className="btn btn-block btn-info w-full text-black bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center"
+                      className="btn btn-block btn-info w-full text-yellow-100 bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center"
                       style={{ cursor: 'pointer' }}
                       type="submit"
                     >
@@ -99,7 +99,7 @@ const Signup = () => {
                 )}
 
                 {error && (
-                  <div className="my-3 p-3 bg-danger text-black text-center">
+                  <div className="my-3 p-3 bg-danger text-yellow-100 text-center">
                     {error.message}
                   </div>
                 )}
